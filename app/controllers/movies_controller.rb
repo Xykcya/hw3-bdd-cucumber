d0f53217f3ce8096613ba08f93f6deb64727f9cf
Nikita <justxykc@gmail.com>
2015-11-22 07:26:22 +0000

    files added

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,107 @@
+class MoviesController < ApplicationController
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    #@all_ratings = Movie.uniq.pluck(:rating)#ratings arr
+    @all_ratings = ['G','PG','PG-13','R']
+    #ratings
+    @selected_ratings = []
+    @sort             = ""
+    redirect          = false
+
+
+    
+    if(params[:ratings])
+      params[:ratings].each {|key, value| @selected_ratings << key}
+      session[:ratings] = @selected_ratings
+    elsif session[:ratings]
+        @selected_ratings = session[:ratings]
+        redirect = true
+      else
+        @selected_ratings = ['G','PG','PG-13','R']
+    end
+
+    if params[:sort]
+      @sort = params[:sort]
+      session[:sort] = @sort
+    elsif session[:sort]
+      @sort = session[:sort]
+      redirect = true
+    else
+      @sort = []
+    end
+    
+    #style
+      if @sort == 'title'
+        @css_title = 'hilite'
+      elsif @sort == 'release_date'
+        @css_release_date = 'hilite'
+      end
+    #
+    
+     if redirect
+
+      redirect_to movies_path :ratings=>@selected_ratings, :sort=>@sort
+    else
+      @movies = Movie.where(:rating => @selected_ratings).order(@sort)
+    end
+
+=begin
+    if @selected_ratings && @sort
+      @movies = Movie.where(:rating => @selected_ratings).order(@sort).all
+    elsif @selected_ratings
+      @movies = Movie.where(["rating IN (?)", @selected_ratings])#select with ratings
+      
+    elsif @sort
+      @movies = Movie.order(@sort)#else if sorting by title or date
+      if @sort == 'title'
+        @css_title = 'hilite'
+      elsif @sort == 'release_date'
+        @css_release_date = 'hilite'
+      end
+    else
+      @movies = Movie.all#else get all
+      @selected_ratings = Movie.uniq.pluck(:rating)
+    end
+=end
+    
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+end
